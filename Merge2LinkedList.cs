0abe2c2d3b4069c8e8687094aa2a32468e431546
Yongfang <windowsyyf@gmail.com>
2019-02-16 22:20:37 -0500

    update

--- Merge2LinkedList.cs
+++ Merge2LinkedList.cs
@@ -18,45 +18,30 @@ public class Solution {
     
     static ListNode final;
     
-    public static ListNode MergeTwoLists(ListNode l1, ListNode l2) {
-      //we start with 2 nodes, we need to compare them
+  public  ListNode MergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy=new ListNode(0);//this is for the final output, the dummy will point to l3;
+        ListNode l3=dummy;
         
-      final=l1.val>l2.val?l2:l1;
-        //since we pick the smaller one for head, the head.next need to compare with the bigger one
-      if(final==l1)//l1 is smaller
-      {l1=l1.next;
-          
-      }else{l2=l2.next;}
+        while (l1!=null && l2!=null)
+        {
+            if (l1.val<=l2.val){l3.next=l1; l1=l1.next;}
+            else {l3.next=l2; l2=l2.next;}
+            l3=l3.next;
+        }
         
+        if (l1==null&&l2!=null){l3.next=l2;}
+        if (l2==null&&l1!=null){l3.next=l1;}
+        if (l1==null&l2==null){l3=l3;}
         
-      final.next=Smaller(l1,l2);
-        
-        return final;
+        return dummy.next;
     }
-    
-    private static ListNode Smaller(ListNode l1,ListNode l2)
-    {//check null here
-        if (l1==null&&l2==null)
-        {return null;}
-      
-        if (l1==null)
-        {return l2;}
-        
-        if (l2==null)
-        {return l1;}
-        
-        return l1.val>l2.val?l2:l1;
-
-    }
-    
-    
 }
-
+ 
     public class Merge2LinkedListTest
     {
         [Fact]
         public void Test1()
-        { ListNode l1=new ListNode(1);
+        { ListNode l1=new ListNode(1) ;
         l1.next=new ListNode(2);
         l1.next.next=new ListNode(4);
 
