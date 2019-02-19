71d25294f2832d586e1b8f3644a1e842facc4553
phillip <windowsyyf@gmail.com>
2019-02-19 18:55:41 -0500

    test

--- RemoveElement.cs
+++ RemoveElement.cs
@@ -2,23 +2,22 @@ using Xunit;
 
 namespace RemoveElement{
 public class Solution {
+    //original solution
     public int RemoveElement(int[] nums, int val) {
         
         int i=0;//starting index
         int j=nums.Length-1;//ending index
 
         if (nums.Length==0) return 0;
-        while (i<j)
+        while (i<=j)
         {
             if (val==nums[i])
             {
                 if (val==nums[j])
-                {
-                    //no swap
+                {   //no swap
                     //move j down
                     j--;
-                }
-                else{
+                }else{
                     //swap with j
                     nums[i]=nums[j];
                     nums[j]=val;
@@ -32,19 +31,31 @@ public class Solution {
             }
         }
         
-        if (nums[j]==val)
-        {
-            return j;
-        }
-        
         return 1+j;
         
         
         
     }
-}
 
-    public class ReverseInteger
+
+//solution online, this solution is easier, but performance wise it is not that good.
+    public int RemoveElement_Online(int[] nums, int val) {
+        
+    int i=0;
+    int j=0;
+        while (j<=nums.Length-1)
+        {
+            if (val!=nums[j])
+            {
+                nums[i]=nums[j];
+                i++;
+            }
+            j++;
+        }
+        return i;
+        
+    }
+    public class RemoveElement
     {
         [Fact]
         public void Test1()
