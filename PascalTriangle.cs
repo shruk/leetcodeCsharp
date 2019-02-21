35032110e0bf5e2245b47fa7fb4e61d2203919a5
phillip <windowsyyf@gmail.com>
2019-02-20 19:10:02 -0500

    test

---
+++ PascalTriangle.cs
@@ -0,0 +1,48 @@
+using System.Collections.Generic;
+using Xunit;
+
+namespace PascalTriangle{
+public class Solution {
+    public IList<IList<int>> Generate(int numRows) {
+        
+        IList<IList<int>> result=new List<IList<int>>();
+        IList<int> temp=new List<int>();
+        
+        
+        
+        for (int i=0;i<numRows;i++)
+        {
+            IList<int> li=new List<int>();//create an empty list for each row
+            for(int j=0;j<=i;j++)
+            {
+                if (j==0 ||j==i)
+                {li.Add(1);}
+                else
+                {
+                    li.Add(temp[j]+temp[j-1]);}
+                
+                
+            }
+            
+            //save li for next round use.
+            temp=li;
+            result.Add(li);
+        }
+        
+        
+        return result;
+        
+        
+    }
+}
+
+    public class PascalTriangleTest
+    {
+        [Fact]
+        public void Test1()
+        { 
+        
+        }
+    }
+
+}
