b3461727ee7035c6725f3bb0ab6cf393833ab4d1
Yongfang <windowsyyf@gmail.com>
2019-02-23 21:02:17 -0500

    test

---
+++ EditDistance72.cs
@@ -0,0 +1,15 @@
+namespace EditDistance72{
+    public class solution{
+            public int MinDistance(string word1, string word2) 
+            {
+        return 0;
+    }
+
+
+    }
+
+
+
+
+
+}
