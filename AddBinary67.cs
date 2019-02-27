2bfba69e312a605fa1fe6b1144b00ed1d8e63d4f
yongfang ye <windowsyyf@gmail.com>
2019-02-27 13:21:04 -0500

    test

---
+++ AddBinary67.cs
@@ -0,0 +1,16 @@
+namespace AddBinary67{
+
+public class Solution{
+    public string AddBinary(string a, string b){
+    //input is non-empty string}
+    //a="11" b="01" sum="100"
+    //a is 3 b is 1 sum is 4 
+    //one method is to convert them into decimal and add, then convert back
+    //convert string to number first
+    int ia=Convert.ToInt32(a);
+    int ib=Convert.ToInt32(b);
+    int sum=ia+ib;
+    return Convert.ToBinary(sum); 
+    }
+	}
+}
