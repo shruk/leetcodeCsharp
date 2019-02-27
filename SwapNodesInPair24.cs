9afaacbc5f5bc286c04bee0cdc749d29cd4e7ff5
yongfang ye <windowsyyf@gmail.com>
2019-02-26 19:30:49 -0500

    test

--- SwapNodesInPair24.cs
+++ SwapNodesInPair24.cs
@@ -2,7 +2,7 @@ namespace SwapNodesInPair{
     public class solution{
     	public ListNode SwapNodes(ListNode head)
 	    {
-	    	//test, this is a c# file
+	    		    	//test, this is a c# file
 	    	//1->2->3->4
 	    	//2->1->4->3
     	ListNode start=new ListNode(0);//start of listnode
@@ -13,9 +13,10 @@ namespace SwapNodesInPair{
     	//start>1->2->3->4
     	temp=start;
     	
-    	while(head.next!=null)
+    	while(head!=null)
 		    {//setups
 			    	//set temp start(temp)->1->2->3->4
+               if(head.next==null){break;}
 		    	next=head.next;//set next 
 		    	next2=next.next;//set next2 
 		    	//(start&&temp)->1(head)->2(next)>3(next2)->4
@@ -31,6 +32,7 @@ namespace SwapNodesInPair{
 	    	}	
     	
     	return start.next;
+    	
     	
     	
     	}
